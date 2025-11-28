5a224369a09718bb8bf562a94747ee5c40cfc4a0
Jnyjy <11002125@uvu.edu>
2025-11-28 14:30:22 -0700

    Fix image path casing for logo and fullsize images

--- Projects/project1/Project7/Myscripts.js
+++ Projects/project1/Project7/Myscripts.js
@@ -1,57 +1,52 @@
 // JavaScript Document
 
-PrintButton1 = new Image
-	PrintButton2 = new Image
-	logo1 = new Image
-	logo2 = new Image
+PrintButton1 = new Image();
+PrintButton2 = new Image();
+logo1 = new Image();
+logo2 = new Image();
 if (document.images) {
-	PrintButton1.src = "images/print5.gif"
-	PrintButton2.src = "images/print6.gif"
-	logo1.src = "images/logo1.jpg"
-	logo2.src = "images/logo2.jpg"
+    PrintButton1.src = "Images/print5.gif";
+    PrintButton2.src = "Images/print6.gif";
+    logo1.src = "Images/logo1.jpg";
+    logo2.src = "Images/logo2.jpg";
 }
 
 
-		var imgArray = new Array(
-			'Image1Lg.jpg',
-			'Image2Lg.jpg',
-			'Image3Lg.jpg',
-			'Image4Lg.jpg'
-			
+var imgArray = new Array(
+    'Image1Lg.jpg',
+    'Image2Lg.jpg',
+    'Image3Lg.jpg',
+    'Image4Lg.jpg'
+);
 
-		);
+var titleArray = new Array(
+    'Biking Couple',
+    'Sunset Couple',
+    'Heli-skiing Couple',
+    'Motorcycle Couple'
+);
 
+var imgPath = "Images/Fullsize/";
 
-		var titleArray = new Array(
-			'Biking Couple',
-			'Sunset Couple',
-			'Heli-skiing Couple',
-			'Motorcycle Couple'
-			
-		);
-			
-		var imgPath = "Images/Fullsize/";
-		
-		function swapImage(imgID) {
+function swapImage(imgID) {
 
-			var theImage = document.getElementById('theImage');
-			var textDiv = document.getElementById('bottomText');
+    var theImage = document.getElementById('theImage');
+    var textDiv = document.getElementById('bottomText');
 
-			var newImg;
-			var textTitle;
+    var newImg;
+    var textTitle;
 
-			newImg = imgArray[imgID];
-			theImage.src = imgPath + newImg;
+    newImg = imgArray[imgID];
+    theImage.src = imgPath + newImg;
 
-			textTitle=titleArray[imgID];
+    textTitle = titleArray[imgID];
 
-      			textDiv.innerHTML = textTitle;
-		}
-			
-		function preloadImages() {		
-			for(var i = 0; i < imgArray.length; i++) {
-				var tmpImg = new Image;
-				tmpImg.src = imgPath + imgArray[i];
-			}
-		}
-		
+    textDiv.innerHTML = textTitle;
+}
+
+function preloadImages() {
+    for (var i = 0; i < imgArray.length; i++) {
+        var tmpImg = new Image();
+        tmpImg.src = imgPath + imgArray[i];
+    }
+}
